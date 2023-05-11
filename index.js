288e1209699bc234e8d420c8d18e4071a3fc532a
Giovannip11 <125603962+Giovannip11@users.noreply.github.com>
2023-05-11 10:47:07 -0300

    aaa

---
+++ index.js
@@ -0,0 +1,13 @@
+function inserir() {
+    var matricula = parseInt(prompt('Digite a matrícula:'));
+    var nome = prompt('Digite o nome:');
+    var faltas = parseInt(prompt('Digite o número de faltas'));
+    var notaB1 = parseFloat(prompt('Digite a nota B1'))
+    var notaB2 = parseFloat(prompt('Digite a nota B2'))
+    var media = (notaB1 + notaB2) / 2;
+    console.log(media);
+
+    else (faltas > 25)
+    if (media >7)
+    alert (prompt)
+}
